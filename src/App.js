de9683936a8fab6a07ffc9f8701a644c980b96c9
Alexander Juárez Pérez <46472434+alexanderjuarezperez@users.noreply.github.com>
2019-11-16 23:34:51 -0600

    cambios nuevos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,57 @@ import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+class Formularios extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email:'',
+      password:''
+    }
+  }
+
+  sincronizarEmail(email){
+    this.setState({
+      email:email
+    })
+  }
+
+  sincronizarPass(password){
+    this.setState({
+      password:password
+    })
+  }
+
+  submitLogin = ()=>{
+    console.log("tu email: "+ this.state.email + " tu password: "+ this.state.password)
+  }
+
+  render() {
+    return (
+       <div>
+         <input 
+         placeholder="Email"
+          type="email"
+          value={this.state.email}
+          onChange={(ev)=>{this.sincronizarEmail(ev.target.value)}}/>
+
+         <input 
+         placeholder="Password"
+         type="password"
+         value={this.state.password}
+         onChange={(ev)=>{this.sincronizarPass(ev.target.value)}}/>
+
+         <h4>Tu nombre es: {this.state.email}</h4>
+         <input
+         type="submit"
+         onClick={this.submitLogin}
+         value="Ingresar"/>
+       </div>
+    );
+  }
+}
+
 class IncrementarState extends Component{
   constructor(props) {
     super(props);
@@ -35,7 +86,7 @@ class App extends Component {
   render(){
     return (
       <div>
-      <IncrementarState/>
+      <Formularios/>
       </div>
       );
     } 
